71efc031db9f6413449beecbbe34e050d33f7820
Mohit kumar <mk513147@gmail.com>
2024-10-01 19:57:04 +0530

    Added animation to delete and edit button.

--- src/Components/TodoCard.js
+++ src/Components/TodoCard.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function TodoCard(props) {
   const {children, handleDeleteTodo, index, handleEditTodo} = props
+  const [isCollapsing, setIsCollapsing] = useState(false);
+
+  const handleDeleteClick = () => {
+    // Trigger collapsing animation
+    setIsCollapsing(true);
+
+    // Wait for the animation to complete before deleting the todo
+    setTimeout(() => {
+      handleDeleteTodo(index);
+    }, 300);
+  };
+
   return (
     <div>
-      <li className='todoItem'>
+      <li className={`todoItem ${isCollapsing ? 'collapsing' : 'slide-up'}`}>
         {children}
         <div className="actionsContainer">
           <button onClick={()=>{
             handleEditTodo(index);
           }}>
           <i className="fa-solid fa-pen-to-square"></i></button>
-          <button onClick={()=>{
-            handleDeleteTodo(index);
-          }}>
-          <i class="fa-solid fa-trash"></i></button>
+          <button onClick={handleDeleteClick}>
+          <i className="fa-solid fa-trash"></i></button>
           </div>
       </li>
     </div>
--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -3,11 +3,19 @@ import React from 'react'
 export default function TodoInput(props) {
     const { handleAddTodos, todoValue, setTodoValue } = props
 
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            handleAddTodos(todoValue);
+            setTodoValue("");
+        }
+    };
+
     return (
         <header>
             <input type="text" value={todoValue} onChange={(e) => {
                 setTodoValue(e.target.value)
-            }} placeholder='Enter todo' />
+            }} onKeyDown={handleKeyPress} placeholder='Enter todo' />
             <button onClick={() => { handleAddTodos(todoValue)
                 setTodoValue("");
              }}>Add</button>
